c6a030760c853e2e45fcdf3fbe59c3567c3ed2da
Andy Weng <andyweng257@gmail.com>
2025-09-30 07:13:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+#file 1
+# for movie in data:
+#     print(movie["title"])
+
+#file 2
+
+useryear = input(" What Year of movies do you want: ")
+
+for movie in data:
+    if movie["year"] <= useryear:
+        print(movie["titl"])
