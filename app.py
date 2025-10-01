03ac1af42a7f423b87e1e8db9f4dd28d46527eb0
Andy Weng <andyweng257@gmail.com>
2025-10-01 06:43:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,27 @@ data = json.load(movies)
 
 #file 2
 
-useryear = input(" What Year of movies do you want: ")
+# useryear = int(input(" What Year of movies do you want: "))
+
+# for movie in data:
+#     if movie["year"] <= useryear:
+#         print(movie["title"])
+
+#file 3
+
+# useryear = int(input("Movies released after:  "))
+# useryearbefore = int(input("Movies released before "))
+
+# for movie in data:
+#     if movie["year"] <= useryear and movie["year"] >= useryearbefore:
+#         print(movie["title"])
+                     
+
+#file 4
+
+useryear = int(input("What year of movies do u want: "))
 
 for movie in data:
-    if movie["year"] <= useryear:
-        print(movie["titl"])
+    if movie["year"] == useryear:
+        print(movie["title"])
+
